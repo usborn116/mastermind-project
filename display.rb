7618308f62a5c61257a3aebbf5c17e63dd2eecff
Usborn Ocampo <usborn116@gmail.com>
2022-06-15 17:09:10 -0700

    some formatting fixes

--- display.rb
+++ display.rb
@@ -22,12 +22,12 @@ class Game
   end
 
   def play
-    if defined?($comp.code)  
-        @code_to_break = $comp.code
-        comp_makes_code
+    if defined?($comp.code)
+      @code_to_break = $comp.code
+      comp_makes_code
     else
-        @code_to_break = $human.code
-        human_makes_code
+      @code_to_break = $human.code
+      human_makes_code
     end
     p code_to_break
   end
@@ -35,17 +35,20 @@ class Game
   def comp_makes_code
     solved = false
     turns = 0
-    while solved == false && turns < 12 
-      p "enter your guess in this format: 1,1,1,1"
-      guess = gets.chomp.split(',').map{|n| n.to_i}
+    p 'You will have 12 chances to guess the computer\'s code. After each guess, you will get feedback on how close your
+    guess was. A number means you guessed the correct number in the correct spot. An "X" means that number is somewhere
+    in the code, but you guessed the wrong spot. An "O" means the number is not in the code at all'
+    while solved == false && turns < 12
+      p 'Enter your guess in this format: 1,1,1,1'
+      guess = gets.chomp.split(',').map { |n| n.to_i }
       @feedback = []
-      guess.each_with_index do |v,i| 
-        if guess[i] == code_to_break[i] 
-        @feedback.push(guess[i])
-        elsif code_to_break.include?(guess[i]) 
-        @feedback.push("X")
+      guess.each_with_index do |_v, i|
+        if guess[i] == code_to_break[i]
+          @feedback.push(guess[i])
+        elsif code_to_break.include?(guess[i])
+          @feedback.push('X')
         else
-        @feedback.push("O")
+          @feedback.push('O')
         end
       end
       turns += 1
@@ -53,45 +56,44 @@ class Game
       p "Guess number #{turns}"
       if feedback == code_to_break
         solved = true
-        p "Congratulations! You guessed it!"
+        p 'Congratulations! You guessed it!'
         break
       end
     end
-    if turns == 12 && feedback != code_to_break
+    return unless turns == 12 && feedback != code_to_break
+
     p "Looks like you didn't get it! Try again?"
-    end
   end
 
-    def human_makes_code
-      solved = false
-      turns = 0
-      guess = Array.new(4)
-      while solved == false && turns < 12
-        guess.each_with_index do |v,i|
-            if guess[i] == code_to_break[i]
-              guess[i]
-            else
-              guess[i] = rand(1..9)
-            end
-        end
-        turns += 1
-        p "Guess number #{turns}"
-        p "The computer guessed #{guess}"
-        p guess
-        p code_to_break
-        sleep(1)
-        if guess == code_to_break
-          solved = true
-          p "The computer guessed it!!"
-          break
+  def human_makes_code
+    solved = false
+    turns = 0
+    guess = Array.new(4)
+    while solved == false && turns < 12
+      guess.each_with_index do |_v, i|
+        if guess[i] == code_to_break[i]
+          guess[i]
+        else
+          guess[i] = rand(1..9)
         end
       end
-      if turns == 12 && feedback != code_to_break
-        p "Nice job! The computer failed!"
+      turns += 1
+      p "Guess number #{turns}"
+      p "The computer guessed #{guess}"
+      p guess
+      p code_to_break
+      sleep(1)
+      if guess == code_to_break
+        solved = true
+        p 'The computer guessed it!!'
+        break
       end
     end
-end
+    return unless turns == 12 && feedback != code_to_break
 
+    p 'Nice job! The computer failed!'
+  end
+end
 
 class CodeGuesser
   attr_accessor :guesser
@@ -125,11 +127,11 @@ class CodeMaker
   end
 
   def generate_code
-    if @maker == 0
-      @code = [rand(1..9),rand(1..9),rand(1..9),rand(1..9)]
+    if @maker.zero?
+      @code = [rand(1..9), rand(1..9), rand(1..9), rand(1..9)]
     else
       puts 'Enter 4 single digit numbers separated by a comma to make as your code'
-      @code = gets.chomp.split(',').map{|n| n.to_i}
+      @code = gets.chomp.split(',').map { |n| n.to_i }
     end
   end
 end
