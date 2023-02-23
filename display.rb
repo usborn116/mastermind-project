62157a6fa8389e34234e128d65ab31640586222a
Usborn Ocampo <usborn116@gmail.com>
2023-02-23 13:33:18 -0800

    refactored quite a bit to be more OOP, and got rid of global varialbes since those are bad practice

--- display.rb
+++ display.rb
@@ -1,141 +1,148 @@
 class Game
-  attr_accessor :guesser, :maker, :code_to_break, :feedback
+
+  attr_accessor :guesser, :maker, :feedback
 
   def initialize
     puts "Welcome to Mastermind! In this game, you will play against a computer to either guess their code,
     or have the computer guess your code!"
+    input
+    @feedback = []
+    play
   end
 
   def input
-    input = ''
-    until input == '1' || input == '2'
-      puts 'Enter 1 to be the code guesser, and 2 to be the code maker'
-      input = gets.chomp
-    end
-    if input == '1'
-      $human = CodeGuesser.new(1)
-      $comp = CodeMaker.new(0)
+    puts 'Enter 1 to be the code guesser or 2 to be the code maker'
+    choice = gets.chomp
+    if choice == '1'
+      self.guesser = CodeGuesser.new(1); self.maker = CodeMaker.new(0)
+    elsif choice == '2'
+      self.maker = CodeMaker.new(1); self.guesser = CodeGuesser.new(0)
     else
-      $human = CodeMaker.new(1)
-      $comp = CodeGuesser.new(0)
+      puts 'Enter 1 or 2!'; input
     end
   end
 
   def play
-    if defined?($comp.code)
-      @code_to_break = $comp.code
-      comp_makes_code
-    else
-      @code_to_break = $human.code
-      human_makes_code
-    end
-    p code_to_break
+    maker.maker.zero? ? human_turns : computer_turns
   end
 
-  def comp_makes_code
-    solved = false
-    turns = 0
-    p 'You will have 12 chances to guess the computer\'s code. After each guess, you will get feedback on how close your
-    guess was. A number means you guessed the correct number in the correct spot. An "X" means that number is somewhere
-    in the code, but you guessed the wrong spot. An "O" means the number is not in the code at all'
-    while solved == false && turns < 12
-      p 'Enter your guess in this format: 1,1,1,1'
-      guess = gets.chomp.split(',').map { |n| n.to_i }
-      @feedback = []
-      guess.each_with_index do |_v, i|
-        if guess[i] == code_to_break[i]
-          @feedback.push(guess[i])
-        elsif code_to_break.include?(guess[i])
-          @feedback.push('X')
-        else
-          @feedback.push('O')
-        end
-      end
-      turns += 1
-      p feedback
-      p "Guess number #{turns}"
-      if feedback == code_to_break
-        solved = true
-        p 'Congratulations! You guessed it!'
-        break
-      end
+  def human_turns
+    while maker.solved == false && guesser.turns < 12
+      p "Guess number #{guesser.turns}. Enter your guess like so: 1,1,1,1"
+      guess = gets.chomp.split(',').map(&:to_i)
+      feedback_update(guess)
+      end_human_turn
     end
-    return unless turns == 12 && feedback != code_to_break
-
-    p "Looks like you didn't get it! Try again?"
   end
 
-  def human_makes_code
-    solved = false
-    turns = 0
+  def feedback_update(guess)
+    self.feedback = []
+    guesser.turns += 1
+    guess.each_with_index do |n, i|
+      if n == maker.code[i]
+        feedback << n
+      elsif maker.code.include?(guess[i])
+        feedback.push('X')
+      else
+        feedback.push('O')
+      end
+    end
+  end
+  
+  def end_human_turn
+    p feedback
+    if feedback == maker.code
+      maker.solved = true
+      p 'Congratulations! You guessed it!'
+      play_again
+    end
+    if guesser.turns == 12 && !maker.solved
+      p "Looks like you didn't get it!" 
+      play_again
+    end
+  end
+
+  def computer_turns
     guess = Array.new(4)
-    while solved == false && turns < 12
+    while maker.solved == false && guesser.turns < 12
       guess.each_with_index do |_v, i|
-        if guess[i] == code_to_break[i]
-          guess[i]
-        else
-          guess[i] = rand(1..9)
-        end
+        guess[i] == maker.code[i] ? guess[i] : guess[i] = rand(1..9)
       end
-      turns += 1
-      p "Guess number #{turns}"
-      p "The computer guessed #{guess}"
-      p guess
-      p code_to_break
-      sleep(1)
-      if guess == code_to_break
-        solved = true
-        p 'The computer guessed it!!'
-        break
-      end
+      comp_guess_update(guess)
     end
-    return unless turns == 12 && feedback != code_to_break
-
+    return unless guesser.turns == 12 && feedback != maker.code
     p 'Nice job! The computer failed!'
+    play_again
+  end
+
+  def comp_guess_update(guess)
+    guesser.turns += 1
+    p "Guess number #{guesser.turns}. The computer guessed #{guess}"
+    sleep(1)
+    end_computer_turn(guess)
+  end
+
+  def end_computer_turn(guess)
+    return unless guess == maker.code
+    maker.solved = true
+    p 'The computer guessed it!!'
+    play_again
+  end
+
+  def play_again
+    p "Play again? Type Y or N"
+    answer = gets.chomp
+    answer == 'Y' ? Game.new : return
   end
 end
 
 class CodeGuesser
-  attr_accessor :guesser
+  attr_accessor :guesser, :turns
 
   def initialize(guesser)
     @guesser = guesser
+    @turns = 0
     startup
   end
 
-  def startup
-    return unless @guesser == 1
+  private
 
-    p 'You are a guesser! You will have 12 chances to guess the computer\'s code.'
+  def startup
+    p 'You are a guesser! You will have 12 chances to guess the computer\'s code.' if guesser == 1
   end
 end
 
 class CodeMaker
-  attr_accessor :maker, :code
+  attr_accessor :maker, :code, :solved
 
   def initialize(maker)
     @maker = maker
-    @code = ''
+    @code = nil
+    @solved = false
     startup
   end
 
   def startup
-    if @maker == 1
-      p 'You are a maker! You will think of a code and the computer will get 12 chances to guess it.'
+    if maker == 1
+      p "You are a maker! You will think of a code and the computer will get 12 chances to guess it. After each guess,
+      you will get feedback on how close your guess was. A number means you guessed the correct number in the
+      correct spot. An 'X' means that number is somewhere in the code, but you guessed the wrong spot. An 'O'
+      means the number is not in the code at all"
     end
     generate_code
   end
 
+  private
+
   def generate_code
-    if @maker.zero?
-      @code = [rand(1..9), rand(1..9), rand(1..9), rand(1..9)]
+    if maker.zero?
+      self.code = [rand(1..9), rand(1..9), rand(1..9), rand(1..9)]
     else
       puts 'Enter 4 single digit numbers separated by a comma to make as your code'
-      @code = gets.chomp.split(',').map { |n| n.to_i }
+      self.code = gets.chomp.split(',').map(&:to_i)
     end
   end
 end
 
 game = Game.new
-game.input
 game.play
